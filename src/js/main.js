3e943c5dab8ae3794389f32117883c48f3a2ea9c
Guilherme <guilhermetavalves@gmail.com>
2019-04-17 11:58:34 -0300

    challengeePlus - estilizando bloco de compra

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,29 @@
-$(".comprar").click(function() {
-    const blocoCompras = document.querySelector(".box-buy");
-    blocoCompras.classList.toggle("ativo");
+var blocoCompras = document.querySelector(".boxBuy");
 
+$(".comprar").click(function() {
+    blocoCompras.classList.toggle("ativo");
 var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-       // Typical action to be performed when the document is ready:
        
        document.getElementById("demo").innerHTML = xhttp.responseText;
+
     }
 };
+
 xhttp.open("GET", "https://guilhermealvestavares.github.io/challenge/products.json", true);
 xhttp.send();
-
-
-
   });
+
+
+
+  $(".menu-button").click(function() {
+   
+    blocoCompras.classList.remove("ativo");
+
+ });
+
+ 
+
+
+
